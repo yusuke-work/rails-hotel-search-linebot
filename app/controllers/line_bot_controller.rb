830310dec5c949fb929d6510c6cfd2cb2f07bc6e
yusuke <yusuke.work.d@gmail.com>
2021-08-16 07:04:34 +0900

    LINEからのPOSTリクエストをRailsで受け取る

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,3 +1,16 @@
 class LineBotController < ApplicationController
-  def collback; end
+  protect_from_forgery except: [:callback]
+
+  def callback
+    body = request.body.read
+  end
+
+  private
+ 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 end
