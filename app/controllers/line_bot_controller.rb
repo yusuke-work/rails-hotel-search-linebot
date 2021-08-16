c139608b2720f4c669f98cea3221c16b3ad168a9
yusuke <yusuke.work.d@gmail.com>
2021-08-16 10:14:17 +0900

    オウム返しBot完成

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,10 +3,30 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private
- 
+
+  # Line::Bot::Clientクラスをインスタンス化することでメッセージの解析や返信などの機能を使うことができるようになる
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
