04d349a189cba671003c57ed3a0339576edb7ae2
yusuke <yusuke.work.d@gmail.com>
2021-08-16 17:54:58 +0900

    エラー処理

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -13,10 +13,7 @@ class LineBotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          message = search_and_create_message(event.message['text'])
           client.reply_message(event['replyToken'], message)
         end
       end
@@ -31,6 +28,37 @@ class LineBotController < ApplicationController
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def search_and_create_message(keyword)
+    http_client = HTTPClient.new
+    url = 'https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426'
+    query = {
+      'keyword' => keyword,
+      'applicationId' => ENV['RAKUTEN_APPID'],
+      'hits' => 5,
+      'responseType' => 'small',
+      'formatVersion' => 2
+    }
+    response = http_client.get(url, query)
+    response = JSON.parse(response.body)
+    
+    if response.key?('error')
+      text = "この検索条件に該当する宿泊施設が見つかりませんでした。\n条件を変えて再検索してください。"
+    else
+      text = ''
+      response['hotels'].each do |hotel|
+        text <<
+        hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+        hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+        "\n"
+      end
+    end
+      
+    message = {
+      type: 'text',
+      text: text
     }
   end
 end
